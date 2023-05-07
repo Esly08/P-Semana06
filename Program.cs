8ceaf9eb7e7ba5e210665c06681ff78fb76c880c
Esly_08 A <125147470+Esly08@users.noreply.github.com>
2023-05-06 23:17:53 -0600

    Do - While
    Uso del ciclo do - while.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Ingrese el número");
+int numero = int.Parse(Console.ReadLine());
+
+int divisor = 2;
+int primo = 0;
+
+do
+{
+    int prueba = numero % divisor;
+    if (prueba == 0)
+    {
+        //True
+        primo++;
+        divisor++;
+    }
+    else
+    {
+        //False
+        divisor++;
+    }
+}
+while (numero != divisor);
+    if (primo == 0)
+    {
+        //True
+        primo++;
+    }
+else
+    {
+        //False
+        divisor++;
+    }
